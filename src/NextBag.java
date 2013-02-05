230b044236701c4b1011c0044f3da8986a0a1608
ajl2612 <ajl2612@gmail.com>
2013-02-05 02:36:24 -0500

    Doc Updates in NextBag and NextBody

--- src/NextBag.java
+++ src/NextBag.java
@@ -1,3 +1,7 @@
-
+/**
+ * Flag that BagScanner is ready for another bag.
+ * 
+ * @author Andrew Lyne
+ */
 public class NextBag {
 }
--- src/NextBody.java
+++ src/NextBody.java
@@ -1,2 +1,6 @@
-
+/**
+ * Flag that BodyScanner is ready for another person.
+ * 
+ * @author Andrew Lyne
+ */
 public class NextBody {}
