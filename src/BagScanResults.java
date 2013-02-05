796905e5e57ca1804a54d03640e038f090c6d786
ajl2612 <ajl2612@gmail.com>
2013-02-05 02:27:27 -0500

    Doc update for scan result containers
    Updated documentation for BagScanResults and BodyScanResults

--- src/BagScanResults.java
+++ src/BagScanResults.java
@@ -1,18 +1,44 @@
+/**
+ * Container class for Perons's Baggage and their BagScan results.
+ * 
+ * @author Andrew Lyne
+ */
 
 public class BagScanResults {
 	
+	/**
+	 * Bags associated with this scan result
+	 */
 	private final Baggage bags;
+	
+	/**
+	 * results of this BaggageScan
+	 */
 	private final boolean didPass;
 	
+	/**
+	 * Constructor for BaggageScanResults
+	 * 
+	 * @param b - b=Baggage
+	 * @param status - results of Baggage scan
+	 */
 	public BagScanResults(Baggage b, boolean status){
 		bags = b;
 		didPass = status;
 	}
 	
+	/**
+	 * Accessor for status of BagScancan 
+	 * @return
+	 */
 	public boolean getStatus(){
 		return didPass;
 	}
 
+	/**
+	 * Accessor for Person
+	 * @return
+	 */
 	public Person getOwner(){
 		return bags.getOwner();
 	}
--- src/BodyScanResults.java
+++ src/BodyScanResults.java
@@ -1,18 +1,43 @@
-
+/**
+ * Container class for Person and their BodyScan results.
+ * 
+ * @author Andrew Lyne
+ */
 public class BodyScanResults {
 	
+	/**
+	 * Person who has just completed BodyScan
+	 */
 	private final Person guest;
+	
+	/**
+	 * Results of BodyScan
+	 */
 	private final boolean didPass;
 	
+	/**
+	 * Constructor for BodyScanResults
+	 * 
+	 * @param p - Person whose results are contained here
+	 * @param status - results of the body scan
+	 */
 	public BodyScanResults(Person p, boolean status){
 		guest = p;
 		didPass = status;
 	}
 	
+	/**
+	 * Accessor for status of BodyScancan 
+	 * @return
+	 */
 	public boolean getStatus(){
 		return didPass;
 	}
 
+	/**
+	 * Accessor for Person
+	 * @return
+	 */
 	public Person getPerson(){
 		return guest;
 	}
